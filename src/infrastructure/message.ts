4690a4b9ffb13b3957dec1d7633acb876b6cc1a9
Jakub Olan <keinsell@protonmail.com>
2023-06-14 01:44:28 +0200

    docs: 📝 add references to `Message`

--- src/infrastructure/message.ts
+++ src/infrastructure/message.ts
@@ -2,22 +2,55 @@ import { UniqueIdentifier } from "src/identifiers/unique-identifier"
 import { kebabSpace } from "src/utilities/kebab-space"
 
 export enum MessageType {
+	/**
+	 * @see [Enterprise Integration Patterns](https://www.enterpriseintegrationpatterns.com/patterns/messaging/EventMessage.html)
+	 */
 	EVENT = "EVENT",
+	/**
+	 * @see [Enterprise Integration Patterns](https://www.enterpriseintegrationpatterns.com/patterns/messaging/CommandMessage.html)
+	 */
 	COMMAND = "COMMAND",
+	/**
+	 * @see [Enterprise Integration Patterns](https://www.enterpriseintegrationpatterns.com/patterns/messaging/DocumentMessage.html)
+	 */
 	DOCUMENT = "DOCUMENT",
+	/**
+	 * @see [Enterprise Integration Patterns](https://www.enterpriseintegrationpatterns.com/patterns/messaging/RequestReply.html)
+	 */
 	REQUEST = "REQUEST",
+	/**
+	 * @see [Enterprise Integration Patterns](https://www.enterpriseintegrationpatterns.com/patterns/messaging/RequestReply.html)
+	 */
 	REPLY = "REPLY",
 }
 
 /**
- * The Message interface defines a common structure for messages that are exchanged between different services in a
- * message-based architecture.
+ * The Message interface defines a common structure for messages
+ * that are exchanged between different services in a message-based
+ * architecture.
  */
 interface MessageProperties {
 	readonly _id: string
-	/** ﻿`causationId` is an identifier used in event-driven architectures to track the causal relationship between events. It represents the ID of the event that caused the current event to occur. This can be useful for tracing and debugging issues in distributed systems, as it allows developers to see the sequence of events that led to a particular state or behavior. */
+	/** ﻿`causationId` is an identifier used in event-driven architectures to track
+	 * the causal relationship between events. It represents the ID of the event that
+	 * caused the current event to occur. This can be useful for tracing and debugging
+	 * issues in distributed systems, as it allows developers to see the sequence of
+	 * events that led to a particular state or behavior.
+	 *
+	 * @see [RailsEventStore](https://railseventstore.org/docs/v2/correlation_causation/)
+	 * @see [thenativeweb/commands-events/#1](https://github.com/thenativeweb/commands-events/issues/1#issuecomment-385862281)
+	 */
 	readonly _causationId?: UniqueIdentifier | undefined
-	/** A correlation ID is a unique identifier used to correlate and track a specific transaction or event as it moves through a distributed system or across different components. It helps to trace the flow of a request and its associated responses across different services and systems. Correlation IDs can be generated and propagated automatically by software components or added manually by developers for debugging and troubleshooting purposes. */
+	/** A correlation ID is a unique identifier used to correlate and track a
+	 * specific transaction or event as it moves through a distributed system or
+	 * across different components. It helps to trace the flow of a request and
+	 * its associated responses across different services and systems. Correlation IDs
+	 * can be generated and propagated automatically by software components or added
+	 * manually by developers for debugging and troubleshooting purposes.
+	 *
+	 * @see [RailsEventStore](https://railseventstore.org/docs/v2/correlation_causation/)
+	 * @see [thenativeweb/commands-events/#1](https://github.com/thenativeweb/commands-events/issues/1#issuecomment-385862281)
+	 */
 	readonly _correlationId?: UniqueIdentifier | undefined
 	readonly _timestamp: Date
 	readonly _type: MessageType
@@ -53,6 +86,7 @@ export abstract class Message<T = unknown> implements MessageProperties {
 		this._metadata = message?._metadata
 	}
 
+	/** Gets the kebab-cased name of the message class. */
 	static get _name(): string {
 		return kebabSpace(this.constructor.name)
 	}
