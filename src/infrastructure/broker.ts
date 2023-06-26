87feb4ecb721d1ddd2b54f8c85398e8ffb383bb8
Jakub Olan <keinsell@protonmail.com>
2023-06-26 22:05:23 +0200

    refactor: ♻️ simplify `Broker`

--- src/infrastructure/broker.ts
+++ src/infrastructure/broker.ts
@@ -1,29 +1,26 @@
-import { EventEmitter } from "node:events"
-import { Message } from "../messages/message"
-import { Channel } from "../messaging/channel"
-import { Connection } from "../messaging/connection"
-import { Subscriber } from "../messaging/subscriber"
+import {EventEmitter} from "node:events"
+import {Message} from "../messages/message"
+import {Channel} from "../messaging/channel"
+import {Connection} from "../messaging/connection"
+import {Subscriber} from "../messaging/subscriber"
 
 export abstract class Broker<
 	Channels extends Channel<Message>,
-	/** Message brokers typically handle serialized messages rather than deserialized ones. When a message is published to a message broker, it is commonly serialized into a specific format, such as JSON, XML, or binary data, before being sent to the broker. */
-	SERIALIZED_MESSAGE_FORMAT = unknown,
-	/** Message Broker knows about it's channels and should not be used when channel doesn't exist to avoid error. */
-	CONNECTION = unknown
 > extends EventEmitter {
-	public instance: CONNECTION | undefined
+	public instance: unknown | undefined
 
-	constructor(connection?: Connection<CONNECTION>) {
+	constructor(connection?: Connection) {
 		super()
 		this.instance = connection?.instance
 	}
 
 	abstract publish<C extends Channels>(
 		channel: C,
-		message: SERIALIZED_MESSAGE_FORMAT
+		message: unknown,
+		config?: { metadata: Message["_metadata"], headers: Message["_headers"] }
 	): Promise<void> | void
 
-	abstract acknowledge(message: SERIALIZED_MESSAGE_FORMAT): Promise<void> | void
+	abstract acknowledge(message: unknown): Promise<void> | void
 
 	abstract subscribe<C extends Channels>(
 		channel: C,
