3f71b9779c6250cefcc76a5d4d49bd17d7e82038
Jakub Olan <keinsell@protonmail.com>
2023-06-27 23:15:17 +0200

    feat: ✨ add `CUID` identifier

---
+++ src/identifiers/index.ts
@@ -0,0 +1,7 @@
+import {CUID, cuid} from "./cuid/cuid";
+import {sequentialId, SequentialId} from "./sequential-id/sequential-id";
+import {UniqueIdentifier} from "./unique-identifier";
+
+
+export type { UniqueIdentifier, SequentialId, CUID}
+export {sequentialId, cuid}
--- src/identifiers/unique-identifier.ts
+++ src/identifiers/unique-identifier.ts
@@ -1,3 +1,4 @@
-import { SequentialId } from "./sequential-id/sequential-id"
+import {CUID} from "./cuid/cuid";
+import {SequentialId} from "./sequential-id/sequential-id"
 
-export type UniqueIdentifier = string | number | bigint | SequentialId
+export type UniqueIdentifier = string | number | bigint | SequentialId | CUID
